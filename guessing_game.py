bf54e83b51a709b5f3332863e24c43932a7d8627
Ranushiperera <ranushiperera85@gmail.com>
2025-05-03 00:15:29 +0530

    Added gives hints if the guess is too high or too low

--- guessing_game.py
+++ guessing_game.py
@@ -2,6 +2,10 @@ import random
 number = random.randint(1, 10)
 print("Guess a number between 1 and 10")
 guess = int(input())
+if guess < number:
+    print("Too low!")
+elif guess > number:
+    print("Too high!")
 if guess == number:
  print("You win!")
 else:
