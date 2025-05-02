06f320b7f5c20322a77db85da90594153ce254d5
Kaveesha003 <kaveeshadinal200@gmail.com>
2025-05-03 02:05:24 +0530

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,16 @@
 import random
-number = random.randint(1, 10)
-print("Guess a number between 1 and 10")
-guess = int(input())
-if guess < number:
-    print("Too low!")
-elif guess > number:
-    print("Too high!")
-if guess == number:
- print("You win!")
-else:
- print(f"Wrong! The number was {number}")
+
+while True:
+    number = random.randint(1, 100)  # Member 2: Changed range to 1–100
+    print("Guess a number between 1 and 100")
+    guess = int(input())
+
+    if guess == number:
+        print("You win!")
+    else:
+        print(f"Wrong! The number was {number}")
+
+    # Member 3: Replay feature
+    print("Play again? (y/n)")
+    if input().lower() != 'y':
+        break
