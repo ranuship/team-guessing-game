1fc30594674457c87fa61e8a8b6bf4c4bd90baad
Tharumila2002 <tharumilathamadi@gmail.com>
2025-05-03 20:24:31 +0530

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,28 +1,10 @@
-import random
-
-wins = 0
-games_played = 0
-
-while True:
-    number = random.randint(1, 100)
-    print("Guess a number between 1 and 100")
-
-    try:
-        guess = int(input())
-    except ValueError:
-        print("That's not a valid number! Try again.")
-        continue
-
-    games_played += 1
-
-    if guess == number:
-        print("You win!")
-        wins += 1
-    else:
-        print(f"Wrong! The number was {number}")
-
-    print(f"Score: {wins} wins out of {games_played} games")
-
-    print("Play again? (y/n)")
-    if input().lower() != 'y':
-        break
+import random 
+number random.randint(1, 10) 
+print("Guess a number between 1 and 10") 
+guess int(input()) 
+if guess < number: print("Too low!") 
+elif guess > number: print("Too high!") 
+if guess == number: 
+print("You win!") 
+else: 
+print(f"Wrong! The number was (number)")
